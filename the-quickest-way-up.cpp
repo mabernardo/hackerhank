e9548fae749114edd22fa31b21cf8e37c7f3e531
Marcio Bernardo <mabernardo@gmail.com>
2015-12-04 17:02:06 -0200

    fixed bugs and passed all tests in hackerhank

--- the-quickest-way-up.cpp
+++ the-quickest-way-up.cpp
@@ -5,7 +5,11 @@
 #include <vector>
 using namespace std;
 
-//#define DEBUG
+#ifdef DEBUG
+    #define LOG( x ) x
+#else
+    #define LOG( x )
+#endif
 
 const int BOARD_SIZE = 100;
 const int DICE_MIN = 1;
@@ -16,33 +20,41 @@ struct Path {
     int end;
 };
 
-bool hasSnake(const vector<Path>& snakes, int pos)
+void print_path(const vector<Path>& paths)
 {
-    for (const Path& s : snakes)
+    cout << "[ ";
+    for (const Path& p : paths)
+        cout << "(p.s=" << p.start << " p.e=" << p.end << ") ";
+    cout << "]" << endl;
+}
+
+bool hasPath(const vector<Path>& paths, int pos, int origin)
+{
+    for (const Path& s : paths)
     {
-        if (s.start == pos)
+        if (s.start == pos && pos != origin)
             return true;
     }
     return false;
 }
 
-int move(int start, int end, const vector<Path>& snakes)
+int move(int start, int end, const vector<Path>& paths)
 {
     int pos = start;
     int moves = 0;
     int dice, dice_value;
     int distance;
 
-    //cout << "  moving from " << pos << " to " << end << endl;
+    LOG( cout << "  moving from " << pos << " to " << end << " [" );
     while (pos < end)
     {
         distance = end - pos;
         dice_value = distance > DICE_MAX ? DICE_MAX : distance;
         for (dice = dice_value; dice >= DICE_MIN - 1; --dice)
         {
-            if (!hasSnake(snakes, pos + dice))
+            if (!hasPath(paths, pos + dice, end))
             {
-                //cout << "   dice " << dice << endl;
+                LOG ( cout << " +" << dice << " " );
                 pos += dice;
                 break;
             }
@@ -54,49 +66,80 @@ int move(int start, int end, const vector<Path>& snakes)
         }
         ++moves;
     }
-    //cout << "  moved " << moves << endl;
+    LOG( cout << "]  moved " << moves << endl );
     return moves;
 }
+int level = 0;
 
-int quickest_way(const vector<Path>& ladders, const vector<Path>& snakes,
-        int start)
+int quickest_way(const vector<Path>& paths, const vector<Path>& passed, int start)
 {
-        int pos = start;
-        int shortest = 100;
-        int moves_to_ladder, moves = 0;
-        int total_moves = 0;
-        int distance;
-        int dice_value;
+    int pos = start;
+    int shortest = BOARD_SIZE;
+    int m, moves = 0;
+    int total_moves = 0;
+    bool deadend;
+    vector<Path> track = passed;
 
-        //cout << "moving from " << start << endl;
-        for (const Path& p : ladders)
+    ++level;
+
+    LOG( cout << "{" << level << "} moving from " << start << endl );
+    for (const Path& p : paths)
+    {
+        if (start < p.start)
         {
-            if (start < p.start)
+            deadend = false;
+            moves = 0;
+            for (const Path& pd : passed)
             {
-                //cout << " climbing ladder [" << p.start << ", " << p.end << "]" << endl;
-                moves_to_ladder = move(start, p.start, snakes);
-                if (moves_to_ladder == -1)
-                    continue;
-
-                moves = quickest_way(ladders, snakes, p.end);
-                if (moves > -1 && moves < shortest)
+                if (p.start == pd.start && p.end == pd.end)
                 {
-                    shortest = moves + moves_to_ladder;
+                    deadend = true;
+                    break;
+                }
+            }
+            if (deadend)
+                continue;
+
+            LOG( cout << "{" << level << "} entering path (" << p.start << ", "
+                << p.end << ")" << endl );
+            m = move(start, p.start, paths);
+            if (m == -1)
+                continue;
+            moves += m;
+
+            track.push_back(p);
+            m = quickest_way(paths, track, p.end);
+            if (m > -1)
+            {
+                moves += m;
+                LOG( cout << "{" << level << "} moves = " << moves << ", shortest = " << shortest << endl );
+                if (moves < shortest)
+                {
+                    shortest = moves; // + moves_to_path;
+                    LOG( cout << "{" << level << "} shortest = " << shortest << endl);
                     pos = p.end;
                 }
             }
         }
+    }
 
-        if (shortest < 100)
-            total_moves += shortest;
-        else
-        {
-            moves = move(pos, BOARD_SIZE, snakes);
-            total_moves = moves > -1 ? total_moves + moves : -1;
-        }
-        //cout << " total_moves = " << total_moves << endl;
+    m = move(start, BOARD_SIZE, paths);
+    if (m > 0 && m < shortest)
+        shortest = m;
 
-        return total_moves;
+    if (shortest < BOARD_SIZE)
+        total_moves = shortest;
+    else
+    {
+        moves = move(pos, BOARD_SIZE, paths);
+        total_moves = moves > -1 ? total_moves + moves : -1;
+    }
+
+    LOG( cout << "{" << level << "} total_moves = " << total_moves << endl );
+    LOG( print_path(track) );
+
+    --level;
+    return total_moves;
 }
 
 int main()
@@ -107,15 +150,15 @@ int main()
     cin >> t;
     while(t--)
     {
-        vector<Path> ladders;
-        vector<Path> snakes;
+        vector<Path> paths;
+        vector<Path> track;
 
         cin >> l;
         while (l--)
         {
             Path p;
             cin >> p.start >> p.end;
-            ladders.push_back(p);
+            paths.push_back(p);
         }
 
         cin >> s;
@@ -124,17 +167,13 @@ int main()
             Path p;
             cin >> p.start >> p.end;
 
-            snakes.push_back(p);
+            paths.push_back(p);
         }
+        #ifdef DEBUG
+        print_path(paths);
+        #endif
 
-        cout << quickest_way(ladders, snakes, 1) << endl;
-#ifdef DEBUG
-        for (Path& p : ladders)
-            cout << "l.s=" << p.start << " l.e=" << p.end << endl;
-
-        for (Path& p : snakes)
-            cout << "s.s=" << p.start << " s.e=" << p.end << endl;
-#endif
+        cout << quickest_way(paths, track, 1) << endl;
     }
     return 0;
 }
